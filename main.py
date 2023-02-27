8322d388ddec8174f75117f39689c197e7b48b6c
Vagan <eritsyan.@yandex.ru>
2023-02-27 13:25:25 +0300

    Docstrings

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""Game module. Select your character and improve your skills."""
 from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Describe attack function."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Describe defence function."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Describe special skill function."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -38,6 +42,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Describe the training process."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Select your character."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
